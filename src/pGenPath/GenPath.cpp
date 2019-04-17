60d9f8b847964ffca91b9ff8d37cd7b02cbb8467
Kristen Railey <kristenrailey@Kristens-MacBook-Pro.local>
2019-04-17 10:00:23 -0400

    got it working, just have to fix the case where it has one point left and it can't get it exactly

--- src/pGenPath/GenPath.cpp
+++ src/pGenPath/GenPath.cpp
@@ -128,29 +128,30 @@ bool GenPath::Iterate()
       int temp_index=0;
       
       m_current_size =m_dist_to_point.size();
-      
-      for (std::vector<double>::iterator k = m_dist_to_point.begin(); k != m_dist_to_point.end(); ++k){
-	std::cout<<"dist: "<<*k<<std::endl;
-	if ((*k>m_visit_radius)){ //Miss                                                   
+      if (m_finished_search ==true){ //added this
+        for (std::vector<double>::iterator k = m_dist_to_point.begin(); k != m_dist_to_point.end(); ++k){
+	  std::cout<<"dist: "<<*k<<std::endl;
+	  if ((*k>m_visit_radius)){ //Miss                                                   
           //Check if already have number                                                     
 	  
-	  std::string id_str = tokStringParse(m_points_ordered[temp_index], "id", ',', '=');
-	  if(std::find(m_id_revisit_points.begin(), m_id_revisit_points.end(), id_str) !=m_id_revisit_points.end()) { 
-	    temp_index++;
-	    std::cout<<"already have it"<<std::endl;
-	  } //already have it                                                             
-	  else{
+	    std::string id_str = tokStringParse(m_points_ordered[temp_index], "id", ',', '=');
+	    if(std::find(m_id_revisit_points.begin(), m_id_revisit_points.end(), id_str) !=m_id_revisit_points.end()) { 
+	      temp_index++;
+	      std::cout<<"already have it"<<std::endl;
+	    } //already have it                                                             
+	    else{
 	    //if unique id., push back                                                    
-	    m_id_revisit_points.push_back(id_str);
+	      m_id_revisit_points.push_back(id_str);
 
 	    
-	    m_revisit_points.push_back(m_points_ordered[temp_index]);
+	      m_revisit_points.push_back(m_points_ordered[temp_index]);
+	      temp_index++;
+	    }
+	  }
+	  else{ //Hit                                                                        
 	    temp_index++;
 	  }
-	}
-	else{ //Hit                                                                        
-	  temp_index++;
-	}
+        }
       }
       std::cout<<"size of revisit points: "<<m_revisit_points.size()<<", finished search: "<<m_finished_search<<std::endl;
       if ((m_revisit_points.size()==0)&&(m_finished_search==true)){ //DONE                   
@@ -195,10 +196,11 @@ bool GenPath::Iterate()
         m_current_size = 0;
         m_previous_size =0;
         m_first_time_regen=false;
+	Notify("FINISHED_SEARCH","false"); // added this
 	Notify("FINISH_STATUS","revisit missed points, first time");
 	m_finished_status = "revisit missed points, first time";
       }
-      else if((m_revisit_points.size()==0)&&(m_first_time_regen==true)){ //DONE              
+      else if((m_revisit_points.size()==0)&&(m_first_time_regen==true)&&(m_finished_search==true)){ //DONE              
         Notify("RETURN","true");
         Notify("SEARCH","false");
         Notify("MISSED_POINTS","false");
